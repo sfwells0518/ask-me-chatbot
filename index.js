6b35970f6507021e8593de10b443c2bc552381e3
Sam Wells <sfwells0518@gmail.com>
2023-08-05 13:31:36 -0500

    make start over button work to clear conversation from db

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { Configuration, OpenAIApi } from "openai";
 import { initializeApp } from 'firebase/app'; 
-import { getDatabase, ref, push, get } from 'firebase/database';
+import { getDatabase, ref, push, get, remove } from 'firebase/database';
 
 const configuration = new Configuration({
   apiKey: import.meta.env.VITE_OpenAI_API_KEY,
@@ -79,6 +79,11 @@ function renderTypewriterText(text) {
   }
   requestAnimationFrame(animate);
 }
+
+document.getElementById("clear-btn").addEventListener("click", () => {
+  remove(conversationInDb);
+  chatbotConversation.innerHTML = '<div class="speech speech-ai">Hey! How can I help you?</div>';
+});
 
 function renderConversationFromDb() {
   get(conversationInDb).then((snapshot) => {
